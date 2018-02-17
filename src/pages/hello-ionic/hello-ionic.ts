66812f9480f81d460b3524dfe6a8f3f69ee0b376
umarfarookm <umarfarook89@gmail.com>
2018-02-18 01:54:21 +0530

    removed error plugins

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
-import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor(private flashlight: Flashlight) {
+  constructor() {
 
   }
 
   onFlashLight(){
-    this.flashlight.switchOn();
+
   }
 
   offFlashLight(){
-    this.flashlight.switchOff();
+
   }
 }
